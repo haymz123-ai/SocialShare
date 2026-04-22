67b98c3ff097764dbf0acba2d2afa6ea5e7d0bcb
souravmaji1 <majisourav255@gmail.com>
2026-04-22 16:15:44 +0530

    u

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
+
+   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-}
+};
 
-module.exports = nextConfig
+export default nextConfig;
